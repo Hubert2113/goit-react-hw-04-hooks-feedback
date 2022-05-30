05d4183adf04811c3aa4871c715fec39aeeeb1dc
Hubert2113 <htumialiss@gmail.com>
2022-05-30 03:21:23 +0200

    sf

--- src/components/Buttons/FeedbackOptions.jsx
+++ src/components/Buttons/FeedbackOptions.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { FeedbackBtn } from './FeedbackOptions.styles';
 
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+export const FeedbackOptions = ({options}) => {
     return (
         <>
-            <FeedbackBtn onClick={options.handleIncrementGood} name='good'>Good</FeedbackBtn>
-            <FeedbackBtn onClick={options.handleIncrementNeutral} name='neutral'>Neutral</FeedbackBtn>
-            <FeedbackBtn onClick={options.handleIncrementBad} name='bad'>Bad</FeedbackBtn>
+            <FeedbackBtn onClick={options.handleIncrementGood}>Good</FeedbackBtn>
+            <FeedbackBtn onClick={options.handleIncrementNeutral}>Neutral</FeedbackBtn>
+            <FeedbackBtn onClick={options.handleIncrementBad}>Bad</FeedbackBtn>
         </>
     );
 }
--- src/components/Buttons/FeedbackOptions.styles.js
+++ src/components/Buttons/FeedbackOptions.styles.js
@@ -7,4 +7,9 @@ export const FeedbackBtn = styled.button`
     box-shadow: 0px 8px 24px -9px rgba(66, 68, 90, 1);
     margin-left: 10px;
     background-color: white;
+
+    &:active{
+        background-color: #335cff;
+        box-shadow: inset 4px 30px 31px -8px rgba(66, 68, 90, 1);
+    }
 `;
